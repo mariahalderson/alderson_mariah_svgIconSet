38b0ae26b1308a215cf125385841ab959122c6ca
Mariah Alderson <mariah.alderson@gmail.com>
2018-02-19 23:26:05 -0400

    add icons, write js functions

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
-
-
-(() ==> {
+(() => {
 	console.log('svg script file');
 
 	let svgs = document.querySelectorAll('svg');
 
+	//log id on click
 	function logThisSVG(){
-	console.log(this.id);
+		console.log(this.id);
 	}
 
-	svgs.forEach(svg => svg.addEventListener('click', logThisSVG));
+	//zoom in 1.5x on mouseover 
+	function zoom(){
+		document.getElementById(this.id).classList.add('zoom');
+	}
 
+	//return to normal zoom on mouseout
+	function removeZoom(){
+		document.getElementById(this.id).classList.remove('zoom');
+	}
+
+
+
+
+	svgs.forEach(svg => svg.addEventListener('click', logThisSVG));
+	svgs.forEach(svg => svg.addEventListener('mouseover', zoom));
+	svgs.forEach(svg => svg.addEventListener('mouseout', removeZoom));
 
 })();
